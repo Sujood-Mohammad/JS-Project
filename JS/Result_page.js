cd323231d66a7d24f6fc18a9c2289c959841cce7
Sujood-Mohammad <99989053+Sujood-Mohammad@users.noreply.github.com>
2022-04-06 16:07:29 +0300

    main

--- JS/Result_page.js
+++ JS/Result_page.js
@@ -1,4 +1,5 @@
 var id = localStorage.getItem("user_id")
+
 if (id === null) {
     location.href = ("/Index.html")
 }
@@ -9,32 +10,43 @@ else {
     var a = x1.userdata[id];
     var a2 = x2.q;
     var result = a.result;
+
+
     let f = document.getElementById("fail");
     let p = document.getElementById("pass");
     let btn = document.getElementById("ans_btn");
-    let ans = document.getElementById("ans_table");
-    document.getElementById("result").innerHTML = " Result : " + "<span>" + result + " / 20 " + "</span>"
+    let Pass_img = document.getElementById("Pass_img");
+    let Fail_imoji = document.getElementById("Fail_imoji");
+   
 
-    if (result >= 10) {
-        p.style.display = "inline";
+
+    document.getElementById("result").innerHTML = " Result : "  + result + " out of  20 " ;
+
+    if (50 >= 10) {
+        p.style.display = "block";
+        Pass_img.style.display = "inline";
+        
     }
     else {
-        f.style.display = "inline";
+        f.style.display = "block";
+        Fail_imoji.style.display = "inline";
+        
     }
 
 
     /***************************************************************** */
     var op = true;
+
     btn.onclick = function showAnswers() {
         if (op) {
+
             for (let i = 0; i < a2.length; i++) {
                 document.getElementById("card-rev").innerHTML += `<div class="card-rev">
-
                  <div class="q">${a2[i].question}</div> 
-                 <div class="a">${"a-" + a2[i].a}</div> 
-                 <div class="b">${"b-" + a2[i].b}</div>
-                 <div class="c">${"c-" + a2[i].c}</div> 
-                 <div class="d">${"d-" + a2[i].d}</div> 
+                 <div class="a">${"a. " + a2[i].a}</div> 
+                 <div class="b">${"b. " + a2[i].b}</div>
+                 <div class="c">${"c. " + a2[i].c}</div> 
+                 <div class="d">${"d. " + a2[i].d}</div> 
                  
                  </div>`
             }
@@ -45,29 +57,33 @@ else {
             var b1 = document.getElementsByClassName("b")
             var c1 = document.getElementsByClassName("c")
             var d1 = document.getElementsByClassName("d")
+
+
             for (i = 0; i < a.answers.length; i++) {
                 if (a.answers[i] == "a") {
                     a1[i].style.backgroundColor = "#ff6c6c"
                     a1[i].style.color = "white"
-                    a1[i].innerHTML += "  your anwser "
+                   // a1[i].innerHTML += "    your anwser "
                 }
                 if (a.answers[i] == "b") {
                     b1[i].style.backgroundColor = "#ff6c6c"
                     b1[i].style.color = "white"
-                    b1[i].innerHTML += "  your anwser "
+                   // b1[i].innerHTML += "    your anwser "
                 }
                 if (a.answers[i] == "c") {
                     c1[i].style.backgroundColor = "#ff6c6c"
                     c1[i].style.color = "white"
-                    c1[i].innerHTML += "  your anwser "
+                   // c1[i].innerHTML += "    your anwser "
                 }
                 if (a.answers[i] == "d") {
                     d1[i].style.backgroundColor = "#ff6c6c"
                     d1[i].style.color = "white"
-                    d1[i].innerHTML += "  your anwser "
+                   // d1[i].innerHTML += "    your anwser "
                 }
             }
+
             for (i = 0; i < a.answers.length; i++) {
+
                 if (a2[i].correct == "a") {
                     a1[i].style.backgroundColor = "#43d843"
                     a1[i].style.color = "white"
@@ -76,7 +92,7 @@ else {
                 if (a2[i].correct == "b") {
                     b1[i].style.backgroundColor = "#43d843"
                     b1[i].style.color = "white"
-                  //  b1[i].innerHTML += "  correct anwser "
+                    //b1[i].innerHTML += "  correct anwser "
                 }
                 if (a2[i].correct == "c") {
                     c1[i].style.backgroundColor = "#43d843"
@@ -96,17 +112,163 @@ else {
 
 
     function home() {
+        
         var id = localStorage.getItem("user_id")
         if (id) {
 
-            localStorage.removeItem("user_id")
-            location.href = "../Index.html"
+            localStorage.removeItem("user_id");
+            location.href = "../Index.html";
+            //location.href = "login.html"
 
         }
-        else {
+        // else {
 
-            location.href = "../Index.html"
+        //     location.href = "../Index.html";
 
-        }
+        // }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var id = localStorage.getItem("user_id")
+// if (id === null) {
+//     location.href = ("/Index.html")
+// }
+// else {
+
+//     var x1 = JSON.parse(localStorage.getItem("users"))
+//     var x2 = JSON.parse(localStorage.getItem("question"))
+//     var a = x1.userdata[id];
+//     var a2 = x2.q;
+//     var result = a.result;
+//     let f = document.getElementById("fail");
+//     let p = document.getElementById("pass");
+//     let btn = document.getElementById("ans_btn");
+//     let ans = document.getElementById("ans_table");
+//     document.getElementById("result").innerHTML = " Result : " + "<span>" + result + " / 20 " + "</span>"
+
+//     if (result >= 10) {
+//         p.style.display = "inline";
+//     }
+//     else {
+//         f.style.display = "inline";
+//     }
+
+
+//     /***************************************************************** */
+//     var op = true;
+//     btn.onclick = function showAnswers() {
+//         if (op) {
+//             for (let i = 0; i < a2.length; i++) {
+//                 document.getElementById("card-rev").innerHTML += `<div class="card-rev">
+
+//                  <div class="q">${a2[i].question}</div> 
+//                  <div class="a">${"a-" + a2[i].a}</div> 
+//                  <div class="b">${"b-" + a2[i].b}</div>
+//                  <div class="c">${"c-" + a2[i].c}</div> 
+//                  <div class="d">${"d-" + a2[i].d}</div> 
+                 
+//                  </div>`
+//             }
+
+//             document.getElementById("card-rev").style.display = "flex";
+//             op = false
+//             var a1 = document.getElementsByClassName("a")
+//             var b1 = document.getElementsByClassName("b")
+//             var c1 = document.getElementsByClassName("c")
+//             var d1 = document.getElementsByClassName("d")
+//             for (i = 0; i < a.answers.length; i++) {
+//                 if (a.answers[i] == "a") {
+//                     a1[i].style.backgroundColor = "#ff6c6c"
+//                     a1[i].style.color = "white"
+//                     a1[i].innerHTML +=" <br> <br> ’’your anwser’’ "
+//                 }
+//                 if (a.answers[i] == "b") {
+//                     b1[i].style.backgroundColor = "#ff6c6c"
+//                     b1[i].style.color = "white"
+//                     b1[i].innerHTML += " <br> <br> ’’your anwser’’ "
+//                 }
+//                 if (a.answers[i] == "c") {
+//                     c1[i].style.backgroundColor = "#ff6c6c"
+//                     c1[i].style.color = "white"
+//                     c1[i].innerHTML +=" <br> <br> ’’your anwser’’ "
+//                 }
+//                 if (a.answers[i] == "d") {
+//                     d1[i].style.backgroundColor = "#ff6c6c";
+//                     d1[i].style.color = "white";
+//                     d1[i].innerHTML +=" <br> <br> ’’your anwser’’ ";
+//                 }
+//             }
+//             for (i = 0; i < a.answers.length; i++) {
+//                 if (a2[i].correct == "a") {
+//                     a1[i].style.backgroundColor = "#43d843"
+//                     a1[i].style.color = "white"
+//                   //  a1[i].innerHTML += "  correct anwser "
+//                 }
+//                 if (a2[i].correct == "b") {
+//                     b1[i].style.backgroundColor = "#43d843"
+//                     b1[i].style.color = "white"
+//                   //  b1[i].innerHTML += "  correct anwser "
+//                 }
+//                 if (a2[i].correct == "c") {
+//                     c1[i].style.backgroundColor = "#43d843"
+//                     c1[i].style.color = "white"
+//                   //  c1[i].innerHTML += "  correct anwser "
+//                 }
+//                 if (a2[i].correct == "d") {
+//                     d1[i].style.backgroundColor = "#43d843"
+//                     d1[i].style.color = "white"
+//                   //  d1[i].innerHTML += "  correct anwser "
+//                 }
+//             }
+//         }
+
+//     }
+
+
+
+//     function home() {
+//         var id = localStorage.getItem("user_id")
+//         if (id) {
+
+//             localStorage.removeItem("user_id")
+//             location.href = "../Index.html"
+
+//         }
+//         else {
+
+//             location.href = "../Index.html"
+
+//         }
+//     }
+// }
--- JS/sasa.js
+++ JS/sasa.js
@@ -183,7 +183,9 @@ if (id) {
         const d_text = document.getElementById('d_text');
         const nextBtn = document.getElementById('submit');
         const finish = document.getElementById('finish');
-        
+        const start =document.getElementById('st');
+        const letGo =document.getElementById('letGo');
+
         let currentQuiz = 0;
         let score = 0;
 
@@ -216,6 +218,7 @@ if (id) {
             return answer;
         }
 
+
         nextBtn.addEventListener('click', () => {
             const answer = getSelected();
             if (answer) {
@@ -228,26 +231,53 @@ if (id) {
 
                 currentQuiz++;
 
-
+             
                 if (currentQuiz == 9) {
-                    nextBtn.innerHTML = '<button id="b" onclick = fin()>Finish</button>';
-                    document.getElementById('b').onclick()={
 
-                    }
+                    nextBtn.innerText = 'Finish';
 
+                    start.addEventListener('click', () =>{
+                        quiz.style.display ='block';
+                        letGo.style.display ='none';
+                        
+                    })
+                    
                 }
+                
+
                 if (currentQuiz == 14) {
-                    nextBtn.innerHTML = '<span><b>You have finished English quiz ,be ready for IQ Quiz</b></span>';
+                    nextBtn.innerText = 'Finish';
+
+                    start.addEventListener('click', () =>{
+                        quiz.style.display ='block';
+                        letGo.style.display ='none';
+                        
+                    })
                 }
+                
+              
+
                 if (currentQuiz == 10) {
-                    //nextBtn.innerHTML ='<button>Next</button>';
+
+                    quiz.style.display ='none';
+                    letGo.style.display ='block'
+
+                    nextBtn.innerText = 'Next';
                     document.getElementById('title').innerHTML = '<h2 style =" font-weight: 900; font-size: xx-large; ">English Test :</h2>  <img src="../img/eng.jpg" width="150px" height="100px">';
                 }
+                
+                
 
                 if (currentQuiz == 15) {
                     document.getElementById('title').innerHTML = '<h2 style =" font-weight: 900; font-size: xx-large; ">IQ Test :</h2>  <img src="../img/IQ.jpg" width="150px" height="100px">';
-                    nextBtn.innerHTML = '<button>Next</button>';
+                    nextBtn.innerText = 'Next';
+
+                        quiz.style.display ='none';
+                        letGo.style.display ='block';
+                        finish.innerText='You are finish English Quiz ,Be ready to IQ Quiz'
                 }
+
+
                 if (currentQuiz < quizData.length) {
                     loadQuiz();
                 } else {
